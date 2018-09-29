a4e36ef1d07cb8118e70c8914936dd8c6397aa8f
Ana Harris <vana1990@gmail.com>
2018-09-29 19:05:29 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.size <= 1 
     return array.join
   else 
-    array.last = last_element
+    last_element = array.pop
     array.join(", ")
     array.push("and #{last_element}")
   end
